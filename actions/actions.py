823eaa6d3d06c0b8075a21e1f9e0c2539d5d1cab
fancyLv <flqzdzxx@163.coom>
2021-01-05 00:34:36 +0800

    add files

---
+++ actions/actions.py
@@ -0,0 +1,33 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.forms import FormAction
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class EntForm(FormAction):
+
+    def name(self) -> Text:
+        return "ent_form"
+
+    @staticmethod
+    def required_slots(tracker: "Tracker") -> List[Text]:
+        pass
+
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
